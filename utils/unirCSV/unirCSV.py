e2b11eaf0b458f67e549824b297d59242e2822fa
Pedro Souza <pedrosport10@gmail.com>
2023-12-11 22:19:40 -0300

    Unir CSV

---
+++ utils/unirCSV/unirCSV.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+def combinar_csvs(diretorio, arquivo_saida):
+    arquivos_csv = [arquivo for arquivo in os.listdir(diretorio) if arquivo.endswith('.csv')]
+
+    if not arquivos_csv:
+        print('Não foram encontrados arquivos CSV no diretório.')
+        return
+
+    dataframes = []
+
+    for arquivo_csv in arquivos_csv:
+        caminho_arquivo = os.path.join(diretorio, arquivo_csv)
+        dados_csv = pd.read_csv(caminho_arquivo)
+        dataframes.append(dados_csv)
+
+    dados_combinados_final = pd.concat(dataframes, ignore_index=True)
+
+    caminho_saida = os.path.join(diretorio, arquivo_saida)
+    dados_combinados_final.to_csv(caminho_saida, index=False)
+
+    print(f'Arquivos CSV combinados com sucesso. O resultado foi salvo em {caminho_saida}.')
+
+
+diretorio_dados = '../../topLinguagens/csv'
+arquivo_saida_combinado = 'CommitsUnicosUnidos.csv'
+combinar_csvs(diretorio_dados, arquivo_saida_combinado)
